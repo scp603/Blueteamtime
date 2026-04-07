86d6bd2d406f1978c6db6534f96a911f338cd072
Gavin <111811459+gavcs@users.noreply.github.com>
2026-04-07 14:20:25 -0400

    fixing TODO messages to passch.ps1
    The goal is to have this script automatically run at the beginning of each competition day to set the passwords automatically and securely so we're prepared at the beginning of each competition. We'll see how much of that I actually implement

--- gavin/passch.ps1
+++ gavin/passch.ps1
@@ -1,7 +1,11 @@
+# TODO: create a list of users, and a list of salted & hashed passwords that are hard coded here
+
 # $url = "https://github.com/scp603/Blueteamtime/blob/main/gavin/passwords.json"
 $url = "http://127.0.0.1/gavin/passwords.json"
 $raw = Invoke-WebRequest -Uri $url
 $encrypted = $raw.Content | ConvertFrom-Json
+
+# TODO: check if each user is part of hard coded list of users here
 
 $keyInput = Read-Host -AsSecureString "Enter key"
 $keyPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
@@ -11,6 +15,7 @@ $keyBytes = [System.Text.Encoding]::UTF8.GetBytes($keyPlain.PadRight(32).Substri
 
 foreach ($user in $encrypted.PSObject.Properties) {
     $securePassword = $user.Value | ConvertTo-SecureString -Key $keyBytes
+    # TODO: get the salted hashed value of the password to see if it's listed within the hard coded passwords
     Set-ADAccountPassword -Identity $user.Name `
         -NewPassword $securePassword `
         -Reset
