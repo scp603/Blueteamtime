0a394d7dd746d16a40fded4738db03d44d4c985a
Gavin <gavinwmcconnell@gmail.com>
2026-04-07 10:59:53 -0700

    working on decrypting passwords to change user passwords on machien

--- gavin/passch.ps1
+++ gavin/passch.ps1
@@ -1,1 +1,14 @@
-$url = "https://"
+# $url = "https://github.com/scp603/Blueteamtime/blob/main/gavin/passwords.json"
+$url = "http://127.0.0.1/gavin/passwords.json"
+$raw = Invoke-WebRequest -Uri $url
+$encrypted = $raw.Content | ConvertFrom-Json
+
+$keyInput = Read-Host -AsSecureString "Enter key"
+$keyPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($keyInput)
+)
+$keyBytes = [System.Text.Encoding]::UTF8.GetBytes($keyPlain.PadRight(32).Substring(0,32))
+
+foreach ($user in $encrypted.PSObject.Properties) {
+    Write-Output $user.Value
+}
