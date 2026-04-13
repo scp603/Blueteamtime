f07a92a626f70fe50db7c8fae0f955c09a674af0
Gavin McConnell <gavinwmcconnell@gmail.com>
2026-04-13 02:33:05 -0400

    linux user password change script needs to be tested

--- gavin/linpascha.sh
+++ gavin/linpascha.sh
@@ -3,9 +3,10 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-crpass = "\$y\$j9T\$Q3jvzVaO/Ptc3GjddxPth0\$EEg2x3uTC4nUAiQb3UhgmZD0EosDDUEpP03o5NnVqIB"
-ntfpass = ""
-otherpass = "\$y\$j9T\$/8V3dgdNZrSkblazAb/S3/\$TmfmSGyqk4pavKbXLZXBJQbnixyt2mn1nssILmCxMfC"
+crpass = "\$y\$j9T\$SzBcDcvwBua7i6sacLAzh/\$e5xfYGZgQwV81f8lrqIaXJ6sopT/5Ie.NwO.l2f9KPC"
+ntfpass = "\$y\$j9T\$Z0Jl8ZgFSeaW3DPtMLvu50\$wC85so4mjqX/tNiD2Pfv8XHwgEeVC65jiCuZ34aTd41"
+dradpass = "\$y\$j9T\$TZYZ3bmCQUOf2UaONGquW/\$Jl1UbglxCvi496v5.aqbPpmScDAFioH/tRPor3eKvM1"
+otherpass = "\$y\$j9T\$0dK0cmlZpi8hro9ZpMNY0/\$a95bihS3AnT1M9Z6i1f7i8MYUmwBe.BBOfxELCO3929"
 
 notouch = @(
     "GREYTEAM",
@@ -19,14 +20,21 @@ notouch = @(
     "SCP073",
     "SCP343",
     "Grey Team",
-    "Gray Team"
+    "Gray Team",
+    "cyberrange",
+    "ntf",
+    "DR_ADMIN"
 )
 
-sudo sed -i "s/^cyberrange:[^:]*:/cyberrange:$crpass:/" /etc/shadow
+sudo sed -i "s|^cyberrange:[^:]*:|cyberrange:$crpass:|" /etc/shadow
 sudo sed -i "s/^ntf:[^:]*:/ntf:$ntfpass:/" /etc/shadow
 
 while IFS=: read -r username _; do
     if [[ ! " ${notouch[@]} " =~ " $username "]]; then
-        sudo sed -i "s/^$username:[^:]*:/$username:$otherpass:/" /etc/shadow
+        if [[username == "DR_ADMIN" ]]; then
+            sudo sed -i "s/^DR_ADMIN:[^:]*:/$username:$otherpass:/" /etc/shadow
+        else
+            sudo sed -i "s/^$username:[^:]*:/$username:$otherpass:/" /etc/shadow
+        fi
     fi
 done < /etc/passwd
